2eb4bec89a2a07a4e98e6065a98d4f47783eda25
AIE Escuela 1077 <escuela1077buenosaires@gmail.com>
2026-08-03 19:19:25 -0300

    Conectar solicitud de actividades al portal

--- aie-hub-sw.js
+++ aie-hub-sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'aie-pages-1077-v33';
+var CACHE_NAME = 'aie-pages-1077-v34';
 var STATIC_FILES = [
   './',
   './index.html',
--- aie-public-config.js
+++ aie-public-config.js
@@ -3,7 +3,7 @@ window.AIE_PUBLIC_CONFIG = {
   githubPagesBasePath: "/escuela1077buenosaires-edu/",
   supabaseUrl: "https://xrcvclltfzhhuiowszla.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyY3ZjbGx0ZnpoaHVpb3dzemxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NDk5MzgsImV4cCI6MjA5NzEyNTkzOH0.QpO_dHL7h05jwRmeTUeinDe4Fm5JsGwp8HJfV2PA9KY",
-  solicitudCreacionUrl: "",
+  solicitudCreacionUrl: "https://script.google.com/macros/s/AKfycbws0wyuUHviRbdM_xzTodvFnMIx3zv878sKsLoWgHnOa5t6HlGcdgKJxj805-sfrOho/exec",
   imagenSuperior: "",
   schoolId: "1077"
 };
